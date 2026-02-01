184986a6da6b09c255ace8bb01de825b5fa0e878
Muskaan Gauba <muskaan@Muskaans-MacBook-Air.local>
2026-02-01 14:44:47 -0700

    Updated protfolio changes in name

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,38 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
+  
   title: "Muskaan | Software Engineer",
-  description: "Portfolio of Muskaan — Backend, Data, and Full-Stack Engineer",
+  description:
+    "Portfolio of Muskaan  — Backend, Data, and Full-Stack Engineer",
+
+  openGraph: {
+    title: "Muskaan | Software Engineer",
+    description:
+      "Portfolio of Muskaan — Backend, Data, and Full-Stack Engineer",
+    url: "https://muskaan-portfolio-pearl.vercel.app",
+    siteName: "Muskaan Portfolio",
+    images: [
+      {
+        url: "/Muskaan_pic.JPG", // must be in /public
+        width: 800,
+        height: 800,
+        alt: "Muskaan",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Muskaan | Software Engineer",
+    description:
+      "Backend • Data • Full-Stack | Portfolio",
+    images: ["/Muskaan_pic.JPG"],
+  },
+
+
 };
 
 
